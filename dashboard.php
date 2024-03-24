95b7b79eef5852b11993bb4629819dadfd97b756
SinPhourivath <163083945+SinPhourivath@users.noreply.github.com>
2024-03-24 23:23:52 +0700

    Nothing special

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@ try {
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <br>
